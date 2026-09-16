ea61552ed8d533a948dc21810ecb2804887895b6
green <112435622+cwong8751@users.noreply.github.com>
2026-09-16 17:48:34 -0500

    added offline sources
    1. responsive css
    2. nav bar resizing and positional indicator
    3. css3 animation
    4. social media icon + svg icon
    5. basically everything except modal

--- src/js/main.js
+++ src/js/main.js
@@ -45,3 +45,71 @@ nav_experience.addEventListener('click', () => {
 nav_video.addEventListener('click', () => {
     document.getElementById('video').scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
+
+// nav bar position indicator 
+const nav_bar = document.getElementById('nav-bar');
+
+// add a scroll listener, calculate if possition is within. 
+window.addEventListener('scroll', () => {
+    const current_position = window.scrollY;
+    const projects_position = document.getElementById('projects').offsetTop;
+    const experience_position = document.getElementById('experience').offsetTop;
+    const video_position = document.getElementById('video').offsetTop;
+
+    const calc_position = current_position + nav_bar.offsetHeight;
+
+    if(calc_position >= projects_position && calc_position < experience_position) {
+        console.log("scroll position is within projects");
+        document.getElementById('nav-projects').style.backgroundColor = '#6B8E23';
+        document.getElementById('nav-projects').style.transition = 'background-color 0.5s';
+
+        document.getElementById('nav-experience').style.backgroundColor = '#8DB600';
+        document.getElementById('nav-video').style.backgroundColor = '#8DB600';
+    }
+    else if (calc_position >= experience_position && calc_position < video_position) {
+        console.log("scroll position is within experience");
+        document.getElementById('nav-experience').style.backgroundColor = '#6B8E23';
+        document.getElementById('nav-experience').style.transition = 'background-color 0.5s';
+
+        document.getElementById('nav-projects').style.backgroundColor = '#8DB600';
+        document.getElementById('nav-video').style.backgroundColor = '#8DB600';
+    }
+    else if (calc_position >= video_position) {
+        console.log("scroll position is within video");
+        document.getElementById('nav-video').style.backgroundColor = '#6B8E23';
+        document.getElementById('nav-video').style.transition = 'background-color 0.5s';
+
+        document.getElementById('nav-projects').style.backgroundColor = '#8DB600';
+        document.getElementById('nav-experience').style.backgroundColor = '#8DB600';
+    }
+    else{
+        console.log("scroll position is not within any section");
+
+        document.getElementById('nav-projects').style.backgroundColor = '#8DB600';
+        document.getElementById('nav-experience').style.backgroundColor = '#8DB600';
+        document.getElementById('nav-video').style.backgroundColor = '#8DB600';
+    }
+});
+
+// nav bar resizing
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > document.getElementById('nav-bar').offsetHeight) {
+        document.getElementById('nav-bar').style.height = '40px';
+        document.getElementById('nav-bar').style.transition = 'height 0.5s ease-in-out';
+        document.querySelectorAll('#nav-bar li').forEach((li) => {
+            li.style.fontSize = '14px';
+            li.style.transition = 'font-size 0.5s ease-in-out';
+        });
+    }
+
+    // reset it back
+    if(window.scrollY <= document.getElementById('nav-bar').offsetHeight) {
+        document.getElementById('nav-bar').style.height = '50px';
+        document.getElementById('nav-bar').style.transition = 'height 0.5s ease-in-out';
+        document.querySelectorAll('#nav-bar li').forEach((li) => {
+            li.style.fontSize = '18px';
+            li.style.transition = 'font-size 0.5s ease-in-out';
+        });
+    }
+});
